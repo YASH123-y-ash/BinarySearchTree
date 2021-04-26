b1738e6f432ca7e75299b56d42a0c955c10fc790
Yash-y-ash <wardhan696@gmail.com>
2021-04-27 01:47:25 +0530

    Added:creating bst with three element

---
+++ test/java/com/binarysearchtree/BinarySearchTreeTest.java
@@ -0,0 +1,17 @@
+package com.binarysearchtree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTreeTest {
+
+    @Test
+    public void givenElementsWhenAddedToBinarySearchTree_ShouldReturnSize() {
+        BinarySearchTree<Integer> binarySearchTree =  new BinarySearchTree<>();
+        binarySearchTree.add(56);
+        binarySearchTree.add(30);
+        binarySearchTree.add(70);
+        int size = binarySearchTree.getSize();
+        Assertions.assertEquals(3, size);
+    }
+}
